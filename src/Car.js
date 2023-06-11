4748c3731116e3ad24c1ac0e572acf3af1176400
seymzr <seymzr@gmail.com>
2023-06-11 14:27:53 +0400

    First Commit

---
+++ src/Car.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Mesh } from "three";
+
+export function Car() {
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/car/scene2.gltf"
+  );
+  
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(2.5, 0.45, -2);
+    gltf.scene.traverse((object) => {
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+
+  return <primitive object={gltf.scene} />;
+}
